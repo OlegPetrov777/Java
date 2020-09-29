11199a70d14c4cb5b5ad786b4a0f2310b55fcefe
OlegPetrov777 <59148657+OlegPetrov777@users.noreply.github.com>
2020-09-29 09:14:50 +0300

    Update N1.java

--- HomeWork_24_09_20/N1.java
+++ HomeWork_24_09_20/N1.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
+// №1
 public class N1 {
     public static void main(String[] args) {
         getLetterMap("аабабаббабадддцафщовшыайапвщаиав");
@@ -22,5 +22,22 @@ public class N1 {
         for(Map.Entry<Character, Integer> entri: listToSort){
             System.out.println(entri.getKey() + " " + entri.getValue());
         }
+    }
+    
+    // №2
+    public static <K, V> Map<V, HashSet<K>> revertedMap(HashMap<? extends K, ? extends V> mapToRevert){
+        HashMap<V, HashSet<K>> mapFinal = new HashMap<>();
+        Set<K> keys = (Set<K>) mapToRevert.keySet();
+
+        for (K key: keys){
+            V value = mapToRevert.get(key);
+            mapFinal.compute(value, (v,k) ->{
+                if(k == null)
+                    k = new HashSet<>();
+                k.add(key);
+                return k;
+            });
+        }
+        return mapFinal;
     }
 }
