21d7bedbe25908ed23a684204b32fd91e1f1543d
Oleg <59148657+OlegPetrov777@users.noreply.github.com>
2021-03-21 17:28:40 +0300

    Delete SpringBoot/demo directory

--- SpringBoot/demo/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO person (first_name, last_name, city, street)
-VALUE ('qwe1', 'asd1', 'city1', 'street1'),
-('qwe2', 'asd2', 'city2', 'street2'),
-('qwe3', 'asd3', 'city3', 'street3'),
-('qwe4', 'asd4', 'city4', 'street4')
